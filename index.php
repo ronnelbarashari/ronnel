f1422d7cc113c5683c6639252827c8ed38124a95
test <test@test.com>
2017-10-10 20:09:23 +0800

    test
    test

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <h1>Welcome to RocketScrum!</h1>
     <h3>Scrum is incredibly simple to understand yet incredibly hard to practice.</h3>
     <h3>RocketScrum is your ticket to successful Scrum.</h3>
+    <h3>Testing Github.</h3>
     <div id="firebaseui-auth-container"></div>
 </body>
 <?php
